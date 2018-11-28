bf63eb9036e74ab244c95ae4a6f9018e473837d1
root <root@FM.localdomain>
2018-11-28 13:05:41 +0000

    002

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_125443) do
+ActiveRecord::Schema.define(version: 2018_11_28_130110) do
 
   create_table "employeers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -45,6 +45,18 @@ ActiveRecord::Schema.define(version: 2018_11_28_125443) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["employeer_id"], name: "index_tasks_on_employeer_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "employeers", "stations"
