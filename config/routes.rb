94aa7ac826b598c8deae2c185fd5be99e8abe200
root <root@FM.localdomain>
2018-11-28 13:21:25 +0000

    003

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'welcome/index'
 	
-  root 'welcome#index'
+  root :to => redirect("/admin")
 	
 end
